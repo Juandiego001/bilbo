b98db136c821bf458240e5f18574899e99216eb0
Juandiego001 <juandiego14012003@gmail.com>
2025-01-18 22:38:00 -0500

    fix/info-log-chat-remove-prints - remove prints - add info log to see chat history

--- backend/core/services/chatbot.py
+++ backend/core/services/chatbot.py
@@ -33,20 +33,12 @@ def ai_process_message(number: str):
     try:
         # Procesamiento de IA
         chat = get_or_create_chat_session(number)
-        
         content = rag(combined_message)
         context = prompt(content, combined_message)
         chat.history.append({"role": "user", "parts": combined_message})
-        
-        # print('chat history ah',chat.history)
-        #print('combined_message: ',combined_message)
-        
         response_text = chat.send_message(context).text
-        #print('aqui')
         chat.history.append({"role": "model", "parts": response_text})
-        #print('chat: ', chat)
-        # print('response_text: ',response_text)
-
+        info_logger.info(f'Chat History: {chat.history}')
         info_logger.info(f'Response text: {response_text}')
 
         if '`' in response_text:
